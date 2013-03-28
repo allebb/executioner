6b68493cf2940b9edf736bc1b2d267257a4e6811
Bobby Allen <bobbyallen.uk@gmail.com>
2013-03-28 14:35:05 +0000

    Inital commit of setting up my standard folder structure and configuration of my composer.json file.

---
+++ examples/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Executioner Process Execution Library
+ *
+ * Executioner (executer) is a PHP library for executing system processes
+ * and applications with the ability to pass extra arguments and read
+ *  CLI output results.
+ *
+ * @author bobbyallen.uk@gmail.com (Bobby Allen)
+ * @version 1.0.0
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/bobsta63/executioner
+ *
+ */
+require_once '../src/Ballen/Executioner/Executer.php';
+
+use Ballen\Executioner\Exectuter;
+
+?>
---
+++ src/Ballen/Executioner/Exectuter.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Executioner Process Execution Library
+ *
+ * Executioner (executer) is a PHP library for executing system processes
+ * and applications with the ability to pass extra arguments and read
+ *  CLI output results.
+ *
+ * @author bobbyallen.uk@gmail.com (Bobby Allen)
+ * @version 1.0.0
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/bobsta63/executioner
+ *
+ */
+
+namespace Ballen\Executioner;
+
+class Exectuter
+{
+    //put your code here
+}
+
+?>
