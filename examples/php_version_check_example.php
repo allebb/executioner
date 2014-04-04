f05feab13d851d48558a405bf435107211c9f92c
Bobby Allen <ballen@bobbyallen.me>
2014-04-04 11:51:22 +0100

    Added some new example scripts with comments to show various methods etc.

---
+++ examples/php_version_check_example.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Executioner Process Execution Library
+ *
+ * Executioner is a PHP library for executing system processes
+ * and applications with the ability to pass extra arguments and read
+ *  CLI output results.
+ *
+ * @author ballen@bobbyallen.me (Bobby Allen)
+ * @version 2.0.0
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/bobsta63/executioner
+ *
+ */
+require_once '../src/Ballen/Executioner/Executioner.php';
+
+use Ballen\Executioner\Executioner;
+
+$runner = new Executioner();
+
+
+/**
+ * An example showing how to execute a command and then return the result as an array
+ * and further split infomation down if required.
+ */
+
+
+
+$runner->setApplication('php') // Call the PHP executable to display the version infomation.
+        ->addArgument('-v') // Displays the PHP version number!
+        ->execute();
+
+// We'll grab the result set as an Array as it's quicker and easier to then just grab the first line (1st element of the array)
+$results = $runner->resultAsArray();
+
+
+// We'll now only return the first line (as that is all we really care about, the other ouputted lines are just copyright info etc.)
+echo 'The first line of the output was: <strong>' . $results[0] . '</strong><br>';
+
+// Now we could do some more code to extract only the version from the first line and then just show that!
+
+$words = explode(' ', $results[0]);
+$extracted_version_number = $words[1]; // The second word in the raw outputted line should be the version number :)
+
+echo 'The extacted version number is: <strong>' . $extracted_version_number . '</strong><br>';
---
+++ examples/server_network_info_example.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Executioner Process Execution Library
+ *
+ * Executioner is a PHP library for executing system processes
+ * and applications with the ability to pass extra arguments and read
+ *  CLI output results.
+ *
+ * @author ballen@bobbyallen.me (Bobby Allen)
+ * @version 2.0.0
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/bobsta63/executioner
+ *
+ */
+require_once '../src/Ballen/Executioner/Executioner.php';
+
+use Ballen\Executioner\Executioner;
+
+$runner = new Executioner();
+
+
+/**
+ * An example showing how to view details of a server's network settings eg. ifconfig/ipconfig
+ */
+
+
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    // Windows uses 'ipconfig' to report on the network settings.
+    $runner->setApplication('ipconfig');
+} else {
+    // Linux and UNIX generally uses 'ifconfig' instead...
+    $runner->setApplication('ifconfig');
+}
+
+// We'll now execute the application and get the 'results'
+$runner->execute();
+
+// We'll simply just display the results as plain text...
+echo '<h2>Server NIC settings:</h2>';
+echo '<pre>' .$runner->resultAsText(). '</pre>';
